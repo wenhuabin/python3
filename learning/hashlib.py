55a258c2d273b94cb7ab9a8f8a345f2fe5f54c0f
huabinwen <huabinwen@gmail.com>
2017-09-07 10:04:44 +0800

    add sha1 but not working by run python hashlib.py

--- learning/hashlib.py
+++ learning/hashlib.py
@@ -1,6 +1,11 @@
 import hashlib
 
-#test work on terminal python command
-m = hashlib.md5()
-m.update('Welcome to Merlin!') #return null
-print(m.hexdigest())
+if __name__ == '__main__':
+    #test work on terminal python command
+    #m = hashlib.md5()
+    #m.update('Welcome to Merlin!') #return null
+    #print(m.hexdigest())
+    
+    sha1 = hashlib.sha1()
+    sha1.update('Welcome to Puma')
+    print(sha1.hexdigest())
