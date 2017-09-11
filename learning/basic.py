aedbb00c7f508bdd1c940d178a84a04af280bd70
huabinwen <huabinwen@gmail.com>
2017-09-11 10:02:01 +0800

    keep learning

---
+++ learning/basic.py
@@ -0,0 +1,16 @@
+
+def testWhileElse():
+    count = 0
+    while count < 5:
+        print(count)
+        count += 1
+    else:
+        print(count, 'false')
+
+
+
+
+if __name__ == '__main__':
+    testWhileElse()
+
+
