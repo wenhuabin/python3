738ff784920a603b767d0a287526d346054b4362
huabinwen <huabinwen@gmail.com>
2017-09-12 13:12:11 +0800

    use **

--- learning/basic.py
+++ learning/basic.py
@@ -74,6 +74,10 @@ def main():
 def concat(*args, sep="/"):
     return sep.join(args)
 
+def parrot(voltage, state='a stiff', action='voom'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
 
 
 if __name__ == '__main__':
@@ -101,5 +105,7 @@ if __name__ == '__main__':
 	   #'hello', SyntaxError: positional argument follows keyword argument
            sketch="Cheese Shop Sketch")
     print(concat("earth", "mars", "venus"))
+    d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+    parrot(**d)
 
 
