0e154dae6472c392d6102396f7130c71e5edfdd3
huabinwen <huabinwen@gmail.com>
2017-09-14 10:39:24 +0800

    add json dump and load

--- learning/classjson.py
+++ learning/classjson.py
@@ -13,8 +13,19 @@ def student2dict(std):
         'score': std.score
     }
 
+def dumps(s):
+    with open('/Users/huabin.wen/Puma/p3/python3/files/json.txt', 'w') as f:
+        json.dump(s, f)
+
+def loadDumps():
+    with open('/Users/huabin.wen/Puma/p3/python3/files/json.txt', 'r') as f:
+        print(json.load(f))
+    
+        
 
 s = Student('Bob', 20, 88)
 print(json.dumps(s, default=student2dict))
 print(json.dumps(s, default=lambda obj: obj.__dict__))
+dumps([1,2,3])
+loadDumps()
 
